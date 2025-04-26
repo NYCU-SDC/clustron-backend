ec8b434fbe229fce99ea01eb786f00cb433a1aa7
yukicoder0509 <bendoris0509@gmail.com>
2025-04-26 23:05:30 +0800

    feat: add setting manipulation services

--- internal/setting/queries.sql
+++ internal/setting/queries.sql
@@ -1,14 +1,14 @@
--- name: GetSetting : one
+-- name: GetSetting :one
 SELECT * FROM settings WHERE user_id = $1;
 
--- name: UpdateSetting : one
+-- name: UpdateSetting :one
 UPDATE settings SET username = $2, linux_username = $3 WHERE user_id = $1 RETURNING *;
 
--- name: GetPublicKeys : many
+-- name: GetPublicKeys :many
 SELECT * FROM public_keys WHERE user_id = $1;
 
--- name : AddPublicKey : one
+-- name: AddPublicKey :one
 INSERT INTO public_keys (user_id, keyname, public_key) VALUES ($1, $2, $3) RETURNING *;
 
--- name: DeletePublicKey : exec
+-- name: DeletePublicKey :exec
 DELETE FROM public_keys WHERE user_id = $1 AND keyname = $2;
