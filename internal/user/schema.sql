a0f025a1330d71acdf0f62413fb1779289727a77
Kyle9410-Chen <kyle941010@gmail.com>
2025-04-29 19:20:08 +0800

    feat: add user and refresh token tables with necessary queries and schema

---
+++ internal/user/schema.sql
@@ -0,0 +1,13 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username        VARCHAR(255) UNIQUE NOT NULL,
+    email           VARCHAR(255) UNIQUE NOT NULL,
+    role            VARCHAR(255) DEFAULT 'user',
+    department      VARCHAR(255),
+    student_id      VARCHAR(255) UNIQUE,
+    created_at      TIMESTAMPTZ DEFAULT NOW(),
+    updated_at      TIMESTAMPTZ DEFAULT NOW()
+);
