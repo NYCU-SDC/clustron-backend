9deaea04d01794d3efc8ae71bfe5d9e5b87dbc04
yichen <sunny930817@gmail.com>
2025-05-04 10:05:44 +0800

    feat: update test with table driven

--- internal/ldap/group_test.go
+++ internal/ldap/group_test.go
@@ -7,54 +7,122 @@ import (
 )
 
 func TestGroupFlow(t *testing.T) {
-	groupName := "test_group"
-	gidNumber := "20001"
-	user1 := "test_user1"
-	user2 := "test_user2"
+	type testCase struct {
+		name    string
+		execute func(t *testing.T)
+	}
+
+	const (
+		groupName = "test_group"
+		gidNumber = "20001"
+		user1     = "test_user1"
+		user2     = "test_user2"
+	)
 
 	require.NoError(t, client.CreateUser(user1, "CN1", "SN1", "ssh-rsa AAAA1", "10001"))
 	require.NoError(t, client.CreateUser(user2, "CN2", "SN2", "ssh-rsa AAAA2", "10002"))
 
-	err := client.CreateGroup(groupName, gidNumber, []string{user1})
-	require.NoError(t, err)
+	tests := []testCase{
+		{
+			name: "Should create group successfully",
+			execute: func(t *testing.T) {
+				err := client.CreateGroup(groupName, gidNumber, []string{user1})
+				require.NoError(t, err)
+			},
+		},
+		{
+			name: "Should return ErrGroupNameExists when creating group with duplicate name",
+			execute: func(t *testing.T) {
+				err := client.CreateGroup(groupName, "99999", nil)
+				assert.ErrorIs(t, err, ErrGroupNameExists)
+			},
+		},
+		{
+			name: "Should return ErrGidNumberInUse when creating group with duplicate gidNumber",
+			execute: func(t *testing.T) {
+				err := client.CreateGroup("another_group", gidNumber, nil)
+				assert.ErrorIs(t, err, ErrGidNumberInUse)
+			},
+		},
+		{
+			name: "Should get group info successfully",
+			execute: func(t *testing.T) {
+				entry, err := client.GetGroupInfo(groupName)
+				require.NoError(t, err)
+				assert.Equal(t, groupName, entry.GetAttributeValue("cn"))
+				assert.Contains(t, entry.GetAttributeValues("memberUid"), user1)
+			},
+		},
+		{
+			name: "Should return ErrGroupNotFound when getting info for nonexistent group",
+			execute: func(t *testing.T) {
+				_, err := client.GetGroupInfo("nonexistent")
+				assert.ErrorIs(t, err, ErrGroupNotFound)
+			},
+		},
+		{
+			name: "Should add user to group successfully",
+			execute: func(t *testing.T) {
+				err := client.AddUserToGroup(groupName, user2)
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "Should return ErrUserAlreadyInGroup when adding same user twice",
+			execute: func(t *testing.T) {
+				err := client.AddUserToGroup(groupName, user2)
+				assert.ErrorIs(t, err, ErrUserAlreadyInGroup)
+			},
+		},
+		{
+			name: "Should get groups for user",
+			execute: func(t *testing.T) {
+				groups, err := client.GetGroupsForUser(user2)
+				require.NoError(t, err)
+				assert.GreaterOrEqual(t, len(groups), 1)
+			},
+		},
+		{
+			name: "Should remove user from group successfully",
+			execute: func(t *testing.T) {
+				err := client.RemoveUserFromGroup(groupName, user2)
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "Should return ErrUserNotInGroup when removing nonexistent user",
+			execute: func(t *testing.T) {
+				err := client.RemoveUserFromGroup(groupName, "ghost")
+				assert.ErrorIs(t, err, ErrUserNotInGroup)
+			},
+		},
+		{
+			name: "Should list used gidNumbers including current group",
+			execute: func(t *testing.T) {
+				nums, err := client.GetUsedGidNumbers()
+				require.NoError(t, err)
+				assert.Contains(t, nums, gidNumber)
+			},
+		},
+		{
+			name: "Should delete group successfully",
+			execute: func(t *testing.T) {
+				err := client.DeleteGroup(groupName)
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "Should return error when deleting nonexistent group",
+			execute: func(t *testing.T) {
+				err := client.DeleteGroup("ghost_group")
+				assert.Error(t, err)
+			},
+		},
+	}
 
-	err = client.CreateGroup(groupName, "99999", nil)
-	assert.ErrorIs(t, err, ErrGroupNameExists)
-
-	err = client.CreateGroup("another_group", gidNumber, nil)
-	assert.ErrorIs(t, err, ErrGidNumberInUse)
-
-	entry, err := client.GetGroupInfo(groupName)
-	require.NoError(t, err)
-	assert.Equal(t, groupName, entry.GetAttributeValue("cn"))
-	assert.Contains(t, entry.GetAttributeValues("memberUid"), user1)
-
-	_, err = client.GetGroupInfo("nonexistent")
-	assert.ErrorIs(t, err, ErrGroupNotFound)
-
-	err = client.AddUserToGroup(groupName, user2)
-	assert.NoError(t, err)
-
-	err = client.AddUserToGroup(groupName, user2)
-	assert.ErrorIs(t, err, ErrUserAlreadyInGroup)
-
-	groups, err := client.GetGroupsForUser(user2)
-	require.NoError(t, err)
-	assert.GreaterOrEqual(t, len(groups), 1)
-
-	err = client.RemoveUserFromGroup(groupName, user2)
-	assert.NoError(t, err)
-
-	err = client.RemoveUserFromGroup(groupName, "ghost")
-	assert.ErrorIs(t, err, ErrUserNotInGroup)
-
-	gids, err := client.GetUsedGidNumbers()
-	assert.NoError(t, err)
-	assert.Contains(t, gids, gidNumber)
-
-	err = client.DeleteGroup(groupName)
-	assert.NoError(t, err)
-
-	err = client.DeleteGroup("ghost_group")
-	assert.Error(t, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.execute(t)
+		})
+	}
 }
--- internal/ldap/user_test.go
+++ internal/ldap/user_test.go
@@ -7,64 +7,132 @@ import (
 )
 
 func TestUserFlow(t *testing.T) {
-	uid := "test_user"
-	cn := "Test"
-	sn := "User"
-	uidNumber := "12345"
-	pubkey1 := "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAkey1"
-	pubkey2 := "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAkey2"
+	type testCase struct {
+		name    string
+		execute func(t *testing.T)
+	}
 
-	err := client.CreateUser(uid, cn, sn, pubkey1, uidNumber)
-	require.NoError(t, err)
+	const (
+		uid       = "test_user"
+		uidNumber = "12345"
+		pubkey1   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAkey1"
+		pubkey2   = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAkey2"
+	)
 
-	err = client.CreateUser(uid, "CN", "SN", pubkey1, "54321")
-	assert.ErrorIs(t, err, ErrUserExists)
+	tests := []testCase{
+		{
+			name: "Should create user successfully",
+			execute: func(t *testing.T) {
+				err := client.CreateUser(uid, "Test", "User", pubkey1, uidNumber)
+				require.NoError(t, err)
+			},
+		},
+		{
+			name: "Should return ErrUserExists for duplicate uid",
+			execute: func(t *testing.T) {
+				err := client.CreateUser(uid, "Other", "User", pubkey1, "54321")
+				assert.ErrorIs(t, err, ErrUserExists)
+			},
+		},
+		{
+			name: "Should return ErrUidNumberInUse for duplicate uidNumber",
+			execute: func(t *testing.T) {
+				err := client.CreateUser("another_user", "CN", "SN", pubkey1, uidNumber)
+				assert.ErrorIs(t, err, ErrUidNumberInUse)
+			},
+		},
+		{
+			name: "Should get user info successfully",
+			execute: func(t *testing.T) {
+				entry, err := client.GetUserInfo(uid)
+				require.NoError(t, err)
+				assert.Equal(t, uid, entry.GetAttributeValue("uid"))
+				assert.Equal(t, "Test", entry.GetAttributeValue("cn"))
+			},
+		},
+		{
+			name: "Should return ErrUserNotFound for nonexistent user",
+			execute: func(t *testing.T) {
+				_, err := client.GetUserInfo("nonexistent")
+				assert.ErrorIs(t, err, ErrUserNotFound)
+			},
+		},
+		{
+			name: "Should update user successfully",
+			execute: func(t *testing.T) {
+				err := client.UpdateUser(uid, "UpdatedCN", "UpdatedSN")
+				assert.NoError(t, err)
 
-	err = client.CreateUser("another_user", "CN", "SN", pubkey1, uidNumber)
-	assert.ErrorIs(t, err, ErrUidNumberInUse)
+				entry, _ := client.GetUserInfo(uid)
+				assert.Equal(t, "UpdatedCN", entry.GetAttributeValue("cn"))
+			},
+		},
+		{
+			name: "Should return ErrUserNotFound for update on nonexistent user",
+			execute: func(t *testing.T) {
+				err := client.UpdateUser("ghost", "CN", "SN")
+				assert.ErrorIs(t, err, ErrUserNotFound)
+			},
+		},
+		{
+			name: "Should add SSH public key successfully",
+			execute: func(t *testing.T) {
+				err := client.AddSSHPublicKey(uid, pubkey2)
+				assert.NoError(t, err)
 
-	entry, err := client.GetUserInfo(uid)
-	require.NoError(t, err)
-	assert.Equal(t, uid, entry.GetAttributeValue("uid"))
-	assert.Equal(t, cn, entry.GetAttributeValue("cn"))
+				entry, _ := client.GetUserInfo(uid)
+				assert.Contains(t, entry.GetAttributeValues("sshPublicKey"), pubkey2)
+			},
+		},
+		{
+			name: "Should delete SSH public key successfully",
+			execute: func(t *testing.T) {
+				err := client.DeleteSSHPublicKey(uid, pubkey1)
+				assert.NoError(t, err)
 
-	_, err = client.GetUserInfo("nonexistent")
-	assert.ErrorIs(t, err, ErrUserNotFound)
+				entry, _ := client.GetUserInfo(uid)
+				assert.NotContains(t, entry.GetAttributeValues("sshPublicKey"), pubkey1)
+			},
+		},
+		{
+			name: "Should return ErrUserNotFound when adding key to nonexistent user",
+			execute: func(t *testing.T) {
+				err := client.AddSSHPublicKey("ghost", pubkey1)
+				assert.ErrorIs(t, err, ErrUserNotFound)
+			},
+		},
+		{
+			name: "Should return ErrUserNotFound when deleting key from nonexistent user",
+			execute: func(t *testing.T) {
+				err := client.DeleteSSHPublicKey("ghost", pubkey2)
+				assert.ErrorIs(t, err, ErrUserNotFound)
+			},
+		},
+		{
+			name: "Should list used uidNumbers",
+			execute: func(t *testing.T) {
+				uids, err := client.GetUsedUidNumbers()
+				assert.NoError(t, err)
+				assert.Contains(t, uids, uidNumber)
+			},
+		},
+		{
+			name: "Should delete user successfully",
+			execute: func(t *testing.T) {
+				err := client.DeleteUser(uid)
+				assert.NoError(t, err)
+			},
+		},
+		{
+			name: "Should return ErrUserNotFound when deleting nonexistent user",
+			execute: func(t *testing.T) {
+				err := client.DeleteUser("ghost")
+				assert.ErrorIs(t, err, ErrUserNotFound)
+			},
+		},
+	}
 
-	err = client.UpdateUser(uid, "UpdatedCN", "UpdatedSN")
-	assert.NoError(t, err)
-
-	entry, _ = client.GetUserInfo(uid)
-	assert.Equal(t, "UpdatedCN", entry.GetAttributeValue("cn"))
-
-	err = client.UpdateUser("nonexistent", "CN", "SN")
-	assert.ErrorIs(t, err, ErrUserNotFound)
-
-	err = client.AddSSHPublicKey(uid, pubkey2)
-	assert.NoError(t, err)
-
-	entry, _ = client.GetUserInfo(uid)
-	assert.Contains(t, entry.GetAttributeValues("sshPublicKey"), pubkey2)
-
-	err = client.DeleteSSHPublicKey(uid, pubkey1)
-	assert.NoError(t, err)
-
-	entry, _ = client.GetUserInfo(uid)
-	assert.NotContains(t, entry.GetAttributeValues("sshPublicKey"), pubkey1)
-
-	err = client.AddSSHPublicKey("ghost", pubkey1)
-	assert.ErrorIs(t, err, ErrUserNotFound)
-
-	err = client.DeleteSSHPublicKey("ghost", pubkey2)
-	assert.ErrorIs(t, err, ErrUserNotFound)
-
-	uids, err := client.GetUsedUidNumbers()
-	assert.NoError(t, err)
-	assert.Contains(t, uids, uidNumber)
-
-	err = client.DeleteUser(uid)
-	assert.NoError(t, err)
-
-	err = client.DeleteUser("ghost")
-	assert.ErrorIs(t, err, ErrUserNotFound)
+	for _, tt := range tests {
+		t.Run(tt.name, tt.execute)
+	}
 }
