6b273306d3569da3a0558664631684fd85421263
yukicoder0509 <bendoris0509@gmail.com>
2025-05-06 12:47:02 +0800

    feat: add get all and get by id handler test

--- internal/group/handler_test.go
+++ internal/group/handler_test.go
@@ -8,8 +8,11 @@ import (
 	"github.com/NYCU-SDC/clustron-backend/internal/group"
 	"github.com/NYCU-SDC/clustron-backend/internal/group/mocks"
 	"github.com/NYCU-SDC/clustron-backend/internal/jwt"
+	databaseutil "github.com/NYCU-SDC/summer/pkg/database"
+	"github.com/NYCU-SDC/summer/pkg/pagination"
 	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -90,6 +93,187 @@ func TestHandler_CreateHandler(t *testing.T) {
 			w := httptest.NewRecorder()
 
 			h.CreateHandler(w, r)
+
+			assert.Equal(t, tc.wantStatus, w.Code)
+		})
+	}
+}
+
+func TestHandler_GetAllHandler(t *testing.T) {
+	groups := []group.Group{
+		// organizer, and user in this group
+		{
+			ID:          uuid.MustParse("7942c917-4770-43c1-a56a-952186b9970e"),
+			Title:       "Test Group 1",
+			Description: pgtype.Text{String: "Test Description 1", Valid: true},
+		},
+		// organizer in this group
+		{
+			ID:          uuid.MustParse("7942c917-4770-43c1-a56a-952186b9970f"),
+			Title:       "Test Group 2",
+			Description: pgtype.Text{String: "Test Description 2", Valid: true},
+		},
+		{
+			ID:          uuid.MustParse("7942c917-4770-43c1-a56a-952186b9970a"),
+			Title:       "Test Group 3",
+			Description: pgtype.Text{String: "Test Description 3", Valid: true},
+		},
+	}
+
+	testCases := []struct {
+		name       string
+		user       jwt.User
+		wantStatus int
+		wantResult []string
+	}{
+		{
+			name: "Should get all groups for admin",
+			user: jwt.User{
+				ID:   uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e9"),
+				Role: pgtype.Text{String: "admin", Valid: true},
+			},
+			wantStatus: http.StatusOK,
+			wantResult: []string{
+				"Test Group 1",
+				"Test Group 2",
+				"Test Group 3",
+			},
+		},
+		{
+			name: "Should get limited groups for organizer",
+			user: jwt.User{
+				ID:   uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e3"),
+				Role: pgtype.Text{String: "organizer", Valid: true},
+			},
+			wantStatus: http.StatusOK,
+			wantResult: []string{
+				"Test Group 1",
+				"Test Group 2",
+			},
+		},
+		{
+			name: "Should get limited groups for user",
+			user: jwt.User{
+				ID:   uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e2"),
+				Role: pgtype.Text{String: "user", Valid: true},
+			},
+			wantStatus: http.StatusOK,
+			wantResult: []string{
+				"Test Group 1",
+			},
+		},
+	}
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		t.Fatalf("failed to create logger: %v", err)
+	}
+	store := mocks.NewStore(t)
+
+	// When admin call GetAll
+	store.On("GetAll", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(groups, nil)
+	store.On("GetAllGroupCount", mock.Anything).Return(len(groups), nil)
+
+	// When organizer call GetAll
+	store.On("GetByUserId", mock.Anything, uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e3"), mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(groups[0:2], nil)
+	store.On("GetUserGroupsCount", mock.Anything, uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e3")).Return(2, nil)
+
+	// When user call GetAll
+	store.On("GetByUserId", mock.Anything, uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e2"), mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(groups[0:1], nil)
+	store.On("GetUserGroupsCount", mock.Anything, uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e2")).Return(1, nil)
+
+	auth := mocks.NewAuth(t)
+
+	h := group.NewHandler(validator.New(), logger, store, auth)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := httptest.NewRequest(http.MethodGet, "/groups", nil)
+			r = r.WithContext(context.WithValue(r.Context(), internal.UserContextKey, tc.user))
+			w := httptest.NewRecorder()
+
+			h.GetAllHandler(w, r)
+
+			assert.Equal(t, tc.wantStatus, w.Code)
+
+			var got pagination.Response[group.Response]
+			err := json.Unmarshal(w.Body.Bytes(), &got)
+			if err != nil {
+				t.Fatalf("failed to unmarshal response body: %v", err)
+			}
+
+			for i, g := range got.Items {
+				assert.Equal(t, tc.wantResult[i], g.Title)
+			}
+		})
+	}
+}
+
+func TestHandler_GetByIdHandler(t *testing.T) {
+	testCases := []struct {
+		name       string
+		user       jwt.User
+		wantStatus int
+	}{
+		{
+			name: "Should get group for admin",
+			user: jwt.User{
+				ID:   uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e9"),
+				Role: pgtype.Text{String: "admin", Valid: true},
+			},
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "Should get group for user in this group",
+			user: jwt.User{
+				ID:   uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e2"),
+				Role: pgtype.Text{String: "user", Valid: true},
+			},
+			wantStatus: http.StatusOK,
+		},
+		{
+			name: "Should not get group for user not in this group",
+			user: jwt.User{
+				ID:   uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e5"),
+				Role: pgtype.Text{String: "user", Valid: true},
+			},
+			wantStatus: http.StatusNotFound,
+		},
+	}
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		t.Fatalf("failed to create logger: %v", err)
+	}
+	store := mocks.NewStore(t)
+	// Directly get group by ID
+	store.On("GetById", mock.Anything, uuid.MustParse("7942c917-4770-43c1-a56a-952186b9970e")).Return(group.Group{
+		Title:       "Test Group",
+		Description: pgtype.Text{String: "Test Description", Valid: true},
+	}, nil)
+
+	// Get group by ID for user in this group
+	store.On("FindUserGroupById", mock.Anything, uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e2"), uuid.MustParse("7942c917-4770-43c1-a56a-952186b9970e")).Return(group.Group{
+		Title:       "Test Group",
+		Description: pgtype.Text{String: "Test Description", Valid: true},
+	}, nil)
+
+	// Get group by ID for user not in this group
+	store.On("FindUserGroupById", mock.Anything, uuid.MustParse("a9e0fd99-10de-4ad1-b519-e8430ed089e5"), uuid.MustParse("7942c917-4770-43c1-a56a-952186b9970e")).Return(group.Group{},
+		databaseutil.WrapDBErrorWithKeyValue(pgx.ErrNoRows, "membership", "user_id and group_id", "a9e0fd99-10de-4ad1-b519-e8430ed089e5 7942c917-4770-43c1-a56a-952186b9970e", logger, "get membership"))
+
+	auth := mocks.NewAuth(t)
+
+	h := group.NewHandler(validator.New(), logger, store, auth)
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := httptest.NewRequest(http.MethodGet, "/groups/7942c917-4770-43c1-a56a-952186b9970e", nil)
+			r.SetPathValue("group_id", "7942c917-4770-43c1-a56a-952186b9970e")
+			r = r.WithContext(context.WithValue(r.Context(), internal.UserContextKey, tc.user))
+			w := httptest.NewRecorder()
+
+			h.GetByIdHandler(w, r)
 
 			assert.Equal(t, tc.wantStatus, w.Code)
 		})
