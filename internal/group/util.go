4a6a7c0d4f136688f21cc46be302442565a89c53
yichen <sunny930817@gmail.com>
2025-05-25 11:06:43 +0800

    refactor: update CanAssignRole and HasGroupControlAccess to use context and service methods for access level checks

--- internal/group/util.go
+++ internal/group/util.go
@@ -1,9 +1,41 @@
 package group
 
-func CanAssignRole(myLevel string, targetLevel string) bool {
-	return accessLevelRank[myLevel] > accessLevelRank[targetLevel]
+import (
+	"context"
+
+	logutil "github.com/NYCU-SDC/summer/pkg/log"
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+)
+
+func CanAssignRole(s *Service, ctx context.Context, userId uuid.UUID, groupId uuid.UUID, roleId uuid.UUID) bool {
+	traceCtx, span := s.tracer.Start(ctx, "CanAssignRole")
+	defer span.End()
+	logger := logutil.WithContext(traceCtx, s.logger)
+
+	accessLevel, err := s.GetUserGroupAccessLevel(traceCtx, userId, groupId)
+	if err != nil {
+		logger.Error("failed to get user group access level", zap.Error(err))
+		return false
+	}
+	targetRole, err := s.GetGroupRoleByID(traceCtx, roleId)
+	if err != nil {
+		logger.Error("failed to get member role by id", zap.Error(err))
+		return false
+	}
+	return accessLevelRank[accessLevel] > accessLevelRank[targetRole.AccessLevel]
 }
 
-func HasGroupControlAccess(accessLevel string) bool {
+func HasGroupControlAccess(s *Service, ctx context.Context, userId uuid.UUID, groupId uuid.UUID) bool {
+	traceCtx, span := s.tracer.Start(ctx, "HasGroupControlAccess")
+	defer span.End()
+	logger := logutil.WithContext(traceCtx, s.logger)
+
+	accessLevel, err := s.GetUserGroupAccessLevel(ctx, userId, groupId)
+	if err != nil {
+		logger.Error("failed to get user group access level", zap.Error(err))
+		return false
+	}
+
 	return accessLevel == string(AccessLevelOwner) || accessLevel == string(AccessLevelAdmin)
 }
