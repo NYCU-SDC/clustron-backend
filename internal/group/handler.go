cc4344045fa377b6f9908fe580040a69c1bdfbaf
yichen <sunny930817@gmail.com>
2025-05-26 00:40:36 +0800

    chore: remove comments and update TODO

--- internal/group/handler.go
+++ internal/group/handler.go
@@ -237,17 +237,8 @@ func (h *Handler) CreateHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// 2. Add other members
-	// type AddMemberRequest struct {
-	//	Member string `json:"member"` // email or student id
-	//	Role string `json:"role"`
-	//}
-	// TODO:
-	// errorList := []MemberError{}
+	// TODO: adding errorList to return all the errors in adding members
 	for _, m := range request.Members {
-		// Call AddGroupMember
-		// err != nil
-		//    append to errorList
-		//
 		if m.Member == user.Email || m.Member == user.StudentID.String {
 			continue
 		}
