9541727016bae7171709537d18af03bae1917a2b
Kyle9410-Chen <kyle941010@gmail.com>
2025-06-13 18:13:53 +0800

    test: comment out integrate test cases in group service tests

--- test/integration/group/group_test.go
+++ test/integration/group/group_test.go
@@ -5,8 +5,6 @@ import (
 	dbtestdata "clustron-backend/test/testdata/database"
 	"fmt"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestDemo(t *testing.T) {
@@ -32,62 +30,62 @@ func TestDemo(t *testing.T) {
 }
 
 func TestGroupService_CountAll(t *testing.T) {
-	testCases := []struct {
-		name      string
-		setup     func(t *testing.T, db dbtestdata.DBTX)
-		expect    int64
-		expectErr bool
-	}{
-		{
-			name: "count all groups",
-			setup: func(t *testing.T, db dbtestdata.DBTX) {
-				builder := dbtestdata.NewBuilder(t, db)
-				for i := 0; i < 3; i++ {
-					builder.Group().Create(
-						dbtestdata.GroupWithTitle(fmt.Sprintf("Group %d", i+1)),
-						dbtestdata.GroupWithDescription(fmt.Sprintf("Description for Group %d", i+1)),
-					)
-				}
-			},
-			expect:    3,
-			expectErr: false,
-		},
-		{
-			name:      "count with no groups",
-			expect:    0,
-			expectErr: false,
-		},
-	}
-
-	resourceManager, _, err := integration.GetOrInitResource()
-	if err != nil {
-		t.Fatalf("failed to get resource manager: %v", err)
-	}
-	defer resourceManager.Cleanup()
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			db, rollback, err := resourceManager.SetupPostgres()
-			if err != nil {
-				t.Fatalf("failed to setup postgres: %v", err)
-			}
-			defer rollback()
-
-			// builder := dbtestdata.NewBuilder(t, db)
-			if tc.setup != nil {
-				tc.setup(t, db)
-			}
-
-			//groupService := group.NewService(logger, db)
-
-			if tc.expectErr {
-				require.Error(t, err)
-			} else {
-				require.NoError(t, err)
-				require.Equal(t, tc.expect, 5)
-			}
-		})
-	}
+	//testCases := []struct {
+	//	name      string
+	//	setup     func(t *testing.T, db dbtestdata.DBTX)
+	//	expect    int64
+	//	expectErr bool
+	//}{
+	//	{
+	//		name: "count all groups",
+	//		setup: func(t *testing.T, db dbtestdata.DBTX) {
+	//			builder := dbtestdata.NewBuilder(t, db)
+	//			for i := 0; i < 3; i++ {
+	//				builder.Group().Create(
+	//					dbtestdata.GroupWithTitle(fmt.Sprintf("Group %d", i+1)),
+	//					dbtestdata.GroupWithDescription(fmt.Sprintf("Description for Group %d", i+1)),
+	//				)
+	//			}
+	//		},
+	//		expect:    3,
+	//		expectErr: false,
+	//	},
+	//	{
+	//		name:      "count with no groups",
+	//		expect:    0,
+	//		expectErr: false,
+	//	},
+	//}
+	//
+	//resourceManager, _, err := integration.GetOrInitResource()
+	//if err != nil {
+	//	t.Fatalf("failed to get resource manager: %v", err)
+	//}
+	//defer resourceManager.Cleanup()
+	//
+	//for _, tc := range testCases {
+	//	t.Run(tc.name, func(t *testing.T) {
+	//		db, rollback, err := resourceManager.SetupPostgres()
+	//		if err != nil {
+	//			t.Fatalf("failed to setup postgres: %v", err)
+	//		}
+	//		defer rollback()
+	//
+	//		// builder := dbtestdata.NewBuilder(t, db)
+	//		if tc.setup != nil {
+	//			tc.setup(t, db)
+	//		}
+	//
+	//		//groupService := group.NewService(logger, db)
+	//
+	//		if tc.expectErr {
+	//			require.Error(t, err)
+	//		} else {
+	//			require.NoError(t, err)
+	//			require.Equal(t, tc.expect, 5)
+	//		}
+	//	})
+	//}
 }
 
 func TestGroupService_Create(t *testing.T) {
