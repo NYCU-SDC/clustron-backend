d827e24050abe9721eb900e4b99b551075212c06
yukicoder0509 <bendoris0509@gmail.com>
2025-06-17 21:21:07 +0800

    feat: default group query sort by 'created_at' column

--- internal/group/queries.sql
+++ internal/group/queries.sql
@@ -5,10 +5,10 @@ SELECT COUNT(*) FROM groups;
 SELECT COUNT(*) FROM memberships WHERE user_id = $1;
 
 -- name: ListAscPaged :many
-SELECT * FROM groups ORDER BY @SortBy::text ASC LIMIT @Size OFFSET @Skip;
+SELECT * FROM groups ORDER BY created_at ASC LIMIT @Size OFFSET @Skip;
 
 -- name: ListDescPaged :many
-SELECT * FROM groups ORDER BY @SortBy::text DESC LIMIT @Size OFFSET @Skip;
+SELECT * FROM groups ORDER BY created_at DESC LIMIT @Size OFFSET @Skip;
 
 -- name: ListIfMemberAscPaged :many
 SELECT
