1622e53181335e999ab100ef797d53de83bfc48a
yukicoder0509 <bendoris0509@gmail.com>
2025-07-13 12:54:01 +0800

    fix: test to add the enforcer in the code

--- internal/casbin/enforcer.go
+++ internal/casbin/enforcer.go
@@ -4,6 +4,7 @@ import (
 	"clustron-backend/internal/config"
 	"context"
 	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/model"
 	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
@@ -17,8 +18,29 @@ type Enforcer struct {
 }
 
 func NewEnforcer(logger *zap.Logger, config config.Config) *Enforcer {
+	modelText := `
+[request_definition]
+r = sub, obj, act
+
+[policy_definition]
+p = sub, obj, act
+
+[role_definition]
+g = _, _
+
+[policy_effect]
+e = some(where (p.eft == allow))
+
+[matchers]
+m = g(r.sub, p.sub) && keyMatch2(r.obj, p.obj) && regexMatch(r.act, p.act)
+`
+	m, err := model.NewModelFromString(modelText)
+	if err != nil {
+		logger.Fatal("Failed to create Casbin model", zap.Error(err))
+	}
+
 	// Create a new enforcer with the model and adapter
-	e, err := casbin.NewEnforcer(config.CasbinModelSource, config.CasbinPolicySource)
+	e, err := casbin.NewEnforcer(m, config.CasbinPolicySource)
 	if err != nil {
 		logger.Fatal("Failed to create enforcer", zap.Error(err))
 		return nil
