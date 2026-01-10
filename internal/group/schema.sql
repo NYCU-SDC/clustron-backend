7735b73b125caea741b30f71cbb94c6be9571fe7
Kyle9410-Chen <kyle941010@gmail.com>
2026-01-10 18:11:50 +0800

    feat: implement LDAP group management with base and admin group creation

--- internal/group/schema.sql
+++ internal/group/schema.sql
@@ -5,9 +5,21 @@ CREATE TABLE IF NOT EXISTS groups (
     title VARCHAR(255) NOT NULL,
     description TEXT,
     is_archived BOOLEAN DEFAULT FALSE,
-    gid_number    INTEGER UNIQUE,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TYPE group_type AS ENUM ('Base', 'Admin');
+
+CREATE TABLE IF NOT EXISTS ldap_groups (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    group_id UUID REFERENCES groups(id) NOT NULL,
+    ldap_cn VARCHAR(255) UNIQUE,
+    type group_type NOT NULL,
+    gid_number BIGINT UNIQUE NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    updated_at TIMESTAMPTZ DEFAULT now(),
+    UNIQUE (group_id, type)
 );
 
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
