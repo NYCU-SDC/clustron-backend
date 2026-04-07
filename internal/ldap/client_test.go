f5f0e42e97217f2e2f12dfa49f4f7ca69c6e76b6
SHIRAI117 <shirai117@SHIRAI117deMacBook-Pro.local>
2026-04-07 21:11:22 +0800

    refactor ldap client tests to table driven

--- internal/ldap/client_test.go
+++ internal/ldap/client_test.go
@@ -9,73 +9,99 @@ import (
 )
 
 func TestClient_Validate(t *testing.T) {
-	t.Run("Should validate default People and Groups DNs", func(t *testing.T) {
-		client, done := newTestClient(t)
-		defer done()
+	tests := []struct {
+		name           string
+		userDN         string
+		groupDN        string
+		overrideUserDN string
+		wantErr        bool
+		errContains    string
+	}{
+		{
+			name: "Should validate default People and Groups DNs",
+		},
+		{
+			name:    "Should validate configured user and group DNs",
+			userDN:  "ou=ClustronUsers,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club",
+			groupDN: "ou=ClustronGroups,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club",
+		},
+		{
+			name:           "Should return error when configured user DN does not exist",
+			groupDN:        "ou=ClustronGroups,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club",
+			overrideUserDN: "ou=MissingUsers,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club",
+			wantErr:        true,
+			errContains:    "failed to search for LDAP user DN",
+		},
+	}
 
-		require.NoError(t, client.Validate())
-	})
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			client, done := newTestClientWithDNs(t, tc.userDN, tc.groupDN)
+			defer done()
 
-	t.Run("Should validate configured user and group DNs", func(t *testing.T) {
-		userDN := "ou=ClustronUsers,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club"
-		groupDN := "ou=ClustronGroups,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club"
+			if tc.overrideUserDN != "" {
+				client.Config.LDAPUserDN = tc.overrideUserDN
+			}
 
-		client, done := newTestClientWithDNs(t, userDN, groupDN)
-		defer done()
-
-		require.NoError(t, client.Validate())
-	})
-
-	t.Run("Should return error when configured user DN does not exist", func(t *testing.T) {
-		userDN := "ou=MissingUsers,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club"
-		groupDN := "ou=ClustronGroups,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club"
-
-		client, done := newTestClientWithDNs(t, "", groupDN)
-		defer done()
-
-		client.Config.LDAPUserDN = userDN
-
-		err := client.Validate()
-		require.Error(t, err)
-		assert.Contains(t, err.Error(), "failed to search for LDAP user DN")
-	})
+			err := client.Validate()
+			if tc.wantErr {
+				require.Error(t, err)
+				assert.Contains(t, err.Error(), tc.errContains)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
 }
 
 func TestClient_UsesConfiguredUserAndGroupDNs(t *testing.T) {
-	userDN := "ou=ClustronUsers,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club"
-	groupDN := "ou=ClustronGroups,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club"
+	tests := []struct {
+		name    string
+		userDN  string
+		groupDN string
+	}{
+		{
+			name:    "Should store users and groups under configured DNs",
+			userDN:  "ou=ClustronUsers,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club",
+			groupDN: "ou=ClustronGroups,dc=clustron,dc=prj,dc=internal,dc=sdc,dc=nycu,dc=club",
+		},
+	}
 
-	client, done := newTestClientWithDNs(t, userDN, groupDN)
-	defer done()
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			client, done := newTestClientWithDNs(t, tc.userDN, tc.groupDN)
+			defer done()
 
-	setupUser(t, client, user1, "CN1", "SN1", "ssh-rsa AAAA1", "10001")
-	setupGroup(t, client, groupName, gidNumber, []string{user1})
+			setupUser(t, client, user1, "CN1", "SN1", "ssh-rsa AAAA1", "10001")
+			setupGroup(t, client, groupName, gidNumber, []string{user1})
 
-	userEntry, err := client.GetUserInfo(user1)
-	require.NoError(t, err)
-	assert.Equal(t, "uid="+user1+","+userDN, userEntry.DN)
+			userEntry, err := client.GetUserInfo(user1)
+			require.NoError(t, err)
+			assert.Equal(t, "uid="+user1+","+tc.userDN, userEntry.DN)
 
-	groupEntry, err := client.GetGroupInfo(groupName)
-	require.NoError(t, err)
-	assert.Equal(t, "cn="+groupName+","+groupDN, groupEntry.DN)
+			groupEntry, err := client.GetGroupInfo(groupName)
+			require.NoError(t, err)
+			assert.Equal(t, "cn="+groupName+","+tc.groupDN, groupEntry.DN)
 
-	rawUserEntry, err := client.Conn.Search(ldap.NewSearchRequest(
-		userDN,
-		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		"(uid="+user1+")",
-		[]string{"dn"},
-		nil,
-	))
-	require.NoError(t, err)
-	require.Len(t, rawUserEntry.Entries, 1)
+			rawUserEntry, err := client.Conn.Search(ldap.NewSearchRequest(
+				tc.userDN,
+				ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+				"(uid="+user1+")",
+				[]string{"dn"},
+				nil,
+			))
+			require.NoError(t, err)
+			require.Len(t, rawUserEntry.Entries, 1)
 
-	rawGroupEntry, err := client.Conn.Search(ldap.NewSearchRequest(
-		groupDN,
-		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-		"(cn="+groupName+")",
-		[]string{"dn"},
-		nil,
-	))
-	require.NoError(t, err)
-	require.Len(t, rawGroupEntry.Entries, 1)
+			rawGroupEntry, err := client.Conn.Search(ldap.NewSearchRequest(
+				tc.groupDN,
+				ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+				"(cn="+groupName+")",
+				[]string{"dn"},
+				nil,
+			))
+			require.NoError(t, err)
+			require.Len(t, rawGroupEntry.Entries, 1)
+		})
+	}
 }
